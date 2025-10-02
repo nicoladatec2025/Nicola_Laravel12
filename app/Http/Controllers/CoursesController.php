e82d75ced8c6797b544400d14d5a96018ef4254a
nicoladatec2025 <nicoladatec@gmail.com>
2025-10-02 14:06:51 +0100

    Nova etapa

---
+++ app/Http/Controllers/CoursesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoursesController extends Controller
+{
+    // Listar cursos
+    public function index()
+    {
+        dd("Carregar os cursos");
+    }
+}
